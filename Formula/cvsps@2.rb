4034741de2626617a7336bffa137dd948e026463
Frizlab <fload@me.com>
2017-05-27 13:30:14 +0200

    Update cvsps homepage

--- Formula/cvsps@2.rb
+++ Formula/cvsps@2.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class CvspsAT2 < Formula
-  homepage "http://www.catb.org/~esr/cvsps/"
+  homepage "https://web.archive.org/web/20160127055537/http://www.cobite.com/cvsps/"
   url "https://web.archive.org/web/20160127055537/http://www.cobite.com/cvsps/cvsps-2.1.tar.gz"
   sha256 "91d3198b33463861a581686d5fcf99a5c484e7c4d819384c04fda9cafec1075a"
 
