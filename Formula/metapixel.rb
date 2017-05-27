0a9d9fa13b4aee40defa73324c292a5e37e00281
Frizlab <fload@me.com>
2017-05-27 12:42:56 +0200

    Add metapixel formula

---
+++ Formula/metapixel.rb
@@ -0,0 +1,22 @@
+require "formula"
+
+class Metapixel < Formula
+  homepage "http://www.complang.tuwien.ac.at/schani/metapixel/"
+  url "http://www.complang.tuwien.ac.at/schani/metapixel/files/metapixel-1.0.2.tar.gz"
+  sha256 "8d77810978da397c070b9b4e228ae6204e9f5c524518ad1a4fcab9462171f55b"
+
+  depends_on "jpeg"
+  depends_on "giflib"
+  depends_on "libpng@1.2"
+
+  def install
+    system "make"
+    # The Makefile does not create the man dir
+    mkdir_p man1.to_s
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/metapixel --version"
+  end
+end
