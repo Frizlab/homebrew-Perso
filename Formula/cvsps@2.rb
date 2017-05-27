17c55edd141774e86c6140a0fa4b7e67b088a81e
Frizlab <fload@me.com>
2017-05-27 18:40:12 +0200

    Add a few mirror for the cvsps source tarball

--- Formula/cvsps@2.rb
+++ Formula/cvsps@2.rb
@@ -2,7 +2,9 @@ require "formula"
 
 class CvspsAT2 < Formula
   homepage "https://web.archive.org/web/20160127055537/http://www.cobite.com/cvsps/"
-  url "https://web.archive.org/web/20160127055537/http://www.cobite.com/cvsps/cvsps-2.1.tar.gz"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/c/cvsps/cvsps_2.1.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/c/cvsps/cvsps_2.1.orig.tar.gz"
+  mirror "https://web.archive.org/web/20160127055537/http://www.cobite.com/cvsps/cvsps-2.1.tar.gz"
   sha256 "91d3198b33463861a581686d5fcf99a5c484e7c4d819384c04fda9cafec1075a"
 
   def install
