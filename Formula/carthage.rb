c604a70f0017517fee439dd8ac0d3cecaccf17f6
Frizlab <fload@me.com>
2018-06-05 22:45:54 +0200

    Update Carthage

--- Formula/carthage.rb
+++ Formula/carthage.rb
@@ -2,16 +2,10 @@ class Carthage < Formula
   desc "Decentralized dependency manager for Cocoa"
   homepage "https://github.com/Frizlab/Carthage"
   url "https://github.com/Frizlab/Carthage.git",
-      :tag => "0.27.0",
-      :revision => "f65531d9c3d80875a74655f4a4040030a982b08d",
+      :tag => "0.29.0-Frizlab-Xcode10",
+      :revision => "d394c9f20a81279ca72095f8936c9838167662bb",
       :shallow => false
   head "https://github.com/Frizlab/Carthage.git", :shallow => false
-
-  bottle do
-    cellar :any
-    sha256 "6932d8e5bf91ff66812d438dec5ee4935b1c3cb3d0695940a8b1e49f7a9d541e" => :high_sierra
-    sha256 "e4a1e11c3681944b30c24c7772c661e9bbac55fa6e09ba5b8db055f6d389ee4a" => :sierra
-  end
 
   depends_on :xcode => ["9.0", :build]
 
