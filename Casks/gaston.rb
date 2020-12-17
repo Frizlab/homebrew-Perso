365f37c69a04051ddf0b9459070c4fec527542b7
Frizlab <fload@me.com>
2020-12-17 21:33:27 +0100

    Add Gaston cask

---
+++ Casks/gaston.rb
@@ -0,0 +1,16 @@
+# Latest version on website was 1.2.2
+cask 'gaston' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://leofink.de/gaston/gaston.zip'
+  name 'Gaston'
+  homepage 'http://leofink.de/gaston/'
+
+  app 'Gaston.app'
+
+  zap trash: [
+               '~/Library/Caches/com.leofink.Gaston',
+               '~/Library/Saved Application State/com.leofink.Gaston.savedState',
+             ]
+end
