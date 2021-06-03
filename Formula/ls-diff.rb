3697a6f34df082fa90740ef0d7617dbfe867e3c7
Frizlab <fload@me.com>
2021-06-03 19:32:05 +0200

    Fix audit issues of ls-diff

--- Formula/ls-diff.rb
+++ Formula/ls-diff.rb
@@ -2,11 +2,11 @@ class LsDiff < Formula
   desc "Find file listing diffs between two folders"
   homepage "https://github.com/Frizlab/ls-diff"
   url "https://github.com/Frizlab/ls-diff.git",
-      :tag      => "1.0.0",
-      :revision => "9fc7ddff15445d450c851aff0e9338085e4854dd"
+      tag:      "1.0.0",
+      revision: "9fc7ddff15445d450c851aff0e9338085e4854dd"
   head "https://github.com/Frizlab/ls-diff.git"
 
-  depends_on :xcode => ["12.5", :build]
+  depends_on xcode: ["12.5", :build]
 
   def install
     system "swift", "build", "--disable-sandbox", "--configuration", "release", "--disable-automatic-resolution"
