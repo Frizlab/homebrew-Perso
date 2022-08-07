bdd28862e02cfe9807457eaf6a9efa7ce0566dbc
Frizlab <fload@me.com>
2022-08-07 21:49:27 +0200

    Upgrade LTS Check to 1.0.3

--- Formula/lts-check.rb
+++ Formula/lts-check.rb
@@ -1,7 +1,7 @@
 class LtsCheck < Formula
   desc "Check Long Term Storage Files"
   homepage "https://github.com/Frizlab/lts-check"
-  url "https://github.com/Frizlab/lts-check.git", using: :git, tag: "1.0.2", revision: "b2456e9a08061d0d5de138b66f108e5ebcabece6"
+  url "https://github.com/Frizlab/lts-check.git", using: :git, tag: "1.0.3", revision: "56b8add65a25ebf1c79257884d0aed462e8e11aa"
   head "https://github.com/Frizlab/lts-check.git", using: :git, branch: "develop"
 
   depends_on xcode: ["13.4", :build]
