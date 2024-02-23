21e08aa1ef7099c3f9b9a28f5b353c21773ed6c2
Frizlab <fload@me.com>
2024-02-24 00:58:03 +0100

    Add gpg-pass-from 1.0

---
+++ Formula/gpg-pass-from.rb
@@ -0,0 +1,13 @@
+class GpgPassFrom < Formula
+  desc "Run GPG using password from… something"
+  homepage "https://gist.github.com/Frizlab/fbd3971a42a4fd07765758b095623377"
+  url "https://gist.github.com/fbd3971a42a4fd07765758b095623377.git", using: :git, branch: "main"
+  version "1.0"
+
+  def install
+    files = Dir["./*.sh"]
+    files.each do |f|
+      bin.install f => File.basename(f, File.extname(f))
+	 end
+  end
+end
