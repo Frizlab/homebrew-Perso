356c8f8c1679e91e3e41a78b30a81b8f99a3fe68
Frizlab <fload@me.com>
2025-05-02 16:26:37 +0200

    Update apple-music-to-slack to beta 4

--- Formula/apple-music-to-slack.rb
+++ Formula/apple-music-to-slack.rb
@@ -2,8 +2,8 @@ class AppleMusicToSlack < Formula
   desc "Set your Slack status from the music you’re listening to"
   homepage "https://github.com/Frizlab/apple-music-to-slack"
   url "https://github.com/Frizlab/apple-music-to-slack.git",
-      tag:      "1.0.0-beta.3",
-      revision: "6365e10d954c96614c0f639d5cda699c0e3c2092"
+      tag:      "1.0.0-beta.4",
+      revision: "90964bbef3e61d7874fdcdf47fcd8761ba817b4c"
   head "https://github.com/Frizlab/apple-music-to-slack.git"
 
   depends_on xcode: ["16.2", :build]
